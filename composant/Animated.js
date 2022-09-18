dbb3dfbfba4b62c68f35abf78047848f60a592a0
WardarkXtreme <biard.f.pro@gmail.com>
2022-09-18 22:52:58 +0200

    init logic

--- composant/Animated.js
+++ composant/Animated.js
@@ -1,6 +1,16 @@
 function changeClass(){
-    document.querySelector('.container-group__loader').classList.add('dissolve')
+    document.querySelector('.container-group__loader').classList.add('dissolve');
+    setTimeout(()=>{
+        document.querySelector('.container-group__loader').style.zIndex = '-100';
+    }, 2000);
     document.body.style.overflowY = "auto";
-    console.log('pass')
 }
+const btn = document.querySelectorAll('.btn')
+btn.forEach(btn => {
+    btn.addEventListener('click', (e)=>{
+        window.sessionStorage.setItem("service", `${e.target.id}`)
+        document.location.href = '../public/contact.html';
+    })
+});
+
 setTimeout(changeClass, 5500);
--- index.js
+++
@@ -1,1 +0,0 @@
-import('./composant/Animated.js');
