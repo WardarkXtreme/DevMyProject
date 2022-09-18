aec64fc58bb6ea3be61e00949aa334b3ac78735c
WardarkXtreme <biard.f.pro@gmail.com>
2022-09-18 16:39:07 +0200

    correction test responsive one sur overflowX

--- composant/Animated.js
+++ composant/Animated.js
@@ -1,7 +1,6 @@
 function changeClass(){
     document.querySelector('.container-group__loader').classList.add('dissolve')
-    document.body.style.overflow="auto";
-    document.body.style.overflowX="hidden";
+    document.body.style.overflowY = "auto";
     console.log('pass')
 }
 setTimeout(changeClass, 5500);
