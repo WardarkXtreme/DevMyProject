c91c244144a6358b7d469fb422a5d7303a3f7435
WardarkXtreme <biard.f.pro@gmail.com>
2022-09-28 13:43:25 +0200

    test

--- composant/pageForm.js
+++ composant/pageForm.js
@@ -1,94 +1,141 @@
 const getString = window.sessionStorage.getItem("service")
 const setString = document.querySelector('.text-form').innerHTML = `Vous avez choisie "${getString}"`
-const getFile = document.getElementById('file')
-const btnDel = document.getElementById('delFile')
-const imgPdf = "../stylesheets/asset/pdf.png"
-let addArrayFile=[]
 
+//verify
+const verifyName = document.querySelector('#name');
+const regexName = /^(([a-zA-Z0-9._\s-']{3,25}))$/;
+const verifyMail = document.querySelector('#mail');
+const regexMail = /^(([a-z0-9._-]+@[a-z0-9.]+[.][a-z]{2,6}))$/;
+const verifyNumber = document.querySelector('#number');
+const regexNumber = /^(([0-9]{10}))$/;
 
-function displayFile(){
-    let verify = document.querySelectorAll('.all-file');
-    if(verify != null){
-        const del = document.querySelectorAll('.all-file')
-        del.forEach(div => {
-            div.remove()
-        });
-    };
-
-    addArrayFile.push(getFile.files[0]);
-    let newArrayFile = [...new Set(addArrayFile)];
-    console.log(newArrayFile)
-    newArrayFile.map(files => {
-        let container = document.querySelector('.preview-all__file');
-        let containerAll = document.createElement('div');
-        containerAll.setAttribute('class', 'all-file');
-        let containerAllFilePreview = document.createElement('div');
-        containerAllFilePreview.setAttribute('class', 'all-file__preview');
-        let pictureFilePreview = document.createElement('img');
-        pictureFilePreview.setAttribute('class', 'picture-preview');
-        switch(files.type){
-            case "application/pdf":
-                console.log("test pdf");
-                pictureFilePreview.src = './stylesheets/asset/pdf.png'
-            break;
-            case "video/mp4":
-                console.log("test mp4");
-                pictureFilePreview.src = './stylesheets/asset/mp4.png'
-            break;
-            default: 
-                pictureFilePreview.src = URL.createObjectURL(files);
-            break;
-        }
-        let nameFilePreview = document.createElement('p');
-        nameFilePreview.setAttribute('class', 'name-preview');
-        nameFilePreview.innerHTML = files.name;
-        containerAllFilePreview.appendChild(pictureFilePreview);
-        containerAllFilePreview.appendChild(nameFilePreview);
+verifyName.addEventListener('input', (e)=> {
+    let validName = e.target.value.match(regexName)
+    console.log(e.target.value)
+    let annonce = document.querySelector('.obligation__name');
+    annonce.innerHTML = ""
+    if (!validName){
         
-        containerAll.appendChild(containerAllFilePreview);
-        container.appendChild(containerAll);
-    });
-    console.log(newArrayFile)
-}
-
-function delFile() {
-    addArrayFile = []
-    getFile.files.lenght = 0;
-    let verify = document.querySelectorAll('.all-file');
-    if(verify != null){
-        const del = document.querySelectorAll('.all-file')
-        del.forEach(div => {
-            div.remove()
-        });
-    };
-}
-getFile.addEventListener('change', () => {
-    displayFile();
-});
-btnDel.addEventListener('click', () => {
-    delFile()
+        annonce.innerHTML = "E-mail non valide"
+    }
+    if(e.target.value.length <= 3){
+        
+        annonce.innerHTML = `${e.target.value.length}/3 caractères minimums autorisés`
+    }
+    if( e.target.value.length >= 25){
+        
+        annonce.innerHTML = `${e.target.value.length}/25 caractères maximums autorisés`
+    }
 })
+verifyMail.addEventListener('input', (e)=> {
+    let validMail = e.target.value.match(regexMail)
+    console.log(e.target.value)
+    let annonce = document.querySelector('.obligation__mail');
+    annonce.innerHTML = ""
+    if (!validMail){
+        
+        annonce.innerHTML = "Attention, caractères spéciaux interdits."
+    }
+    if(e.target.value.length <= 3){
+        
+        annonce.innerHTML = `${e.target.value.length}/3 caractères minimums autorisés`
+    }
+    if( e.target.value.length >= 25){
+        
+        annonce.innerHTML = `${e.target.value.length}/25 caractères maximums autorisés`
+    }
+})
+verifyNumber.addEventListener('input', (e)=> {
+    let validNumber = e.target.value.match(regexNumber)
+    console.log(e.target.value)
+    let annonce = document.querySelector('.obligation__number');
+    annonce.innerHTML = ""
+    if (!validNumber){
+
+        annonce.innerHTML = "Attention, Valeure numerique uniquement."
+    }
+    if(e.target.value.length != 10){
+        
+        annonce.innerHTML = `${e.target.value.length}/10 caractères minimums autorisés`
+    }
+})
+
+
+
+//send
 document.querySelector('.btn-send').addEventListener('click', (e)=>{
     e.preventDefault();
-    let verify = document.querySelectorAll('.group-input');
-
-    if(verify != null){
-        const del = document.querySelectorAll('.group-input')
-        let btn = document.querySelector('.btn-input');
-        del.forEach(div => {
-            div.remove()
-        });
-        btn.remove()
-    };
-    const container = document.querySelector(".card-form");
-    const ball = document.createElement('div');
-    ball.setAttribute('class', 'ball');
-    const validIcon = document.createElement('i');
-    validIcon.setAttribute('class', 'fa-solid fa-check valid-ico')
-    const info = document.createElement('h2');
-    info.innerHTML = "Félicitations !"
     
-    ball.appendChild(validIcon);
-    container.appendChild(ball);
-    container.appendChild(info);
+    const name = document.getElementById('name').value;
+    const mail = document.getElementById('mail').value;
+    const number = document.getElementById('number').value;
+    const message = document.getElementById('message').value;
+    
+    let formData = {
+        raison: getString,
+        name: name,
+        mail: mail,
+        number: number,
+        message: message
+    }
+    
+    fetch('http://localhost:3000/api/contact/', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData),
+    })
+    .then((res) => {
+        let verify = document.querySelectorAll('.group-input');
+        console.log("ok")
+        if(verify != null){
+            const del = document.querySelectorAll('.group-input')
+            let btn = document.querySelector('.btn-input');
+            del.forEach(div => {
+                div.remove()
+            });
+            btn.remove()
+        };
+        const container = document.querySelector(".card-form");
+        const ball = document.createElement('div');
+        ball.setAttribute('class', 'ball');
+        const validIcon = document.createElement('i');
+        validIcon.setAttribute('class', 'fa-solid fa-check valid-ico')
+        const info = document.createElement('h2');
+        info.innerHTML = "Félicitations !"
+        const infoTwo = document.createElement('p');
+        infoTwo.innerHTML = "Nous vous répondrons dans les meilleurs délais"
+        ball.appendChild(validIcon);
+        container.appendChild(ball);
+        container.appendChild(info);
+        container.appendChild(infoTwo);
+    })
+    .catch((err) => {
+        console.log(err)
+        let verify = document.querySelectorAll('.group-input');
+        console.log("ko")
+        if(verify != null){
+            const del = document.querySelectorAll('.group-input')
+            let btn = document.querySelector('.btn-input');
+            del.forEach(div => {
+                div.remove()
+            });
+            btn.remove()
+        };
+        const container = document.querySelector(".card-form");
+        const ball = document.createElement('div');
+        ball.setAttribute('class', 'ball');
+        const validIcon = document.createElement('i');
+        validIcon.setAttribute('class', 'fa-solid fa-xmark noValid-ico')
+        const info = document.createElement('h2');
+        info.innerHTML = "Oups !"
+        const infoTwo = document.createElement('p');
+        infoTwo.innerHTML = "Une erreur est survenue, veuillez attendre la redirection..."
+        ball.appendChild(validIcon);
+        container.appendChild(ball);
+        container.appendChild(info);
+        container.appendChild(infoTwo);
+    })
 })
